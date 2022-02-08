038bc192341699c7c9b7de40aaf542f4d6ee068b
RainLiu19980309 <2584307116@qq.com>
2022-02-08 15:31:20 -0500

    update inclass

--- chatapp_front/src/views/Chat.vue
+++ chatapp_front/src/views/Chat.vue
@@ -17,11 +17,21 @@
             <!-- main chat UI -->
             <section id="chat-messages-ui">
                 <!-- custom chat message component here -->
+                <ChatMessage
+                    v-for="msg in messages"
+                    :key="msg.id"
+                    :message="msg.message"
+                    :username="msg.user"
+                />
 
                 <section id="text-wrapper">
-                    <textarea id="message" placeholder="What's on your mind?"></textarea>
+                    <textarea id="message" v-model="message" placeholder="What's on your mind?"></textarea>
 
-                    <button id="sendMessage">Send Message</button>
+                    <button 
+                    id="sendMessage"
+                    @click="sendMessage">
+                        <i class="fa fa-paper-plane" aria-hidden="true"></i>
+                    </button>
                 </section>
             </section>
         </section>
@@ -29,11 +39,60 @@
 </template>
 
 <script>
+import io from "socket.io-client";
+import vars from "@/env.js";
+import ChatMessage from "@/components/ChatMessage.vue";
+
 export default { 
     name:"TheChatComponent",
 
     props: {
         ChatUserName: String
+    },
+
+    mounted() {
+        let vm = this;
+
+        this.socket.on("CONNECTED", (id) => {
+            vm.socketID = id;
+        })
+
+        this.socket.on('MESSAGE', (message) => {
+            // [...] is a the spread operator
+            // short hand way to add something to an array, or put 2 arrays together, etc
+            vm.messages = [...vm.messages, message];
+            console.log(message);
+        })
+    },
+
+    data() {
+        return {
+            socketID: '',
+            message: '',
+            messages: [],
+
+            // set up the socket connection on the front end
+            // this connects to the chat service running on port 3000
+            socket: io(vars.basePath, {
+                withCredentials: false,
+                extraHeaders: {
+                    'Access-Control-Allow-Origin' : '*'
+                }
+            })
+        }
+    },
+
+    methods: {
+        sendMessage() {
+            this.socket.emit('SEND_MESSAGE', { user: this.ChatUserName || "Anonymous", message: this.message });
+
+            // empty out the text area and get ready to input a new message
+            this.message = '';
+        }
+    },
+
+    components: {
+        ChatMessage
     }
 }
 </script>
