0ba7aead5d82e0f05fca9ea50c7d86f1da78d82b
RainLiu19980309 <2584307116@qq.com>
2022-02-28 12:57:11 -0500

    add readme

--- ChatApp_Back/app.js
+++ ChatApp_Back/app.js
@@ -21,6 +21,10 @@ httpServer.listen(port, () => {
 
 // the socket.io stuff will go here - manage incoming connections, sending notifications, managing users, and managing chat messages
 
+// io is the connection manager/switchboard operator - it has access to ALL connections to the chat server and can manage them, broadcast events to everyone, assign IDs etc
+
+// socket is the individual connection to that server - when you join the chat, you're the socket. You can emit an event on the client side (socket) that gets caught on the server side (io) - a message, a typing event, a connection/disconnection event etc. io manages all of those events and can broadcast them to everyone connected to the server
+
 io.on('connection', (socket) => {
     console.log('a user connected!');
 
@@ -30,5 +34,9 @@ io.on('connection', (socket) => {
         console.log('SEND_MESSAGE event!', data);
 
         io.emit('MESSAGE', data);
+    })
+
+    socket.on('USER_TYPING', (data) => {
+        io.emit('SOMEONE_TYPING', data);
     })
 })
